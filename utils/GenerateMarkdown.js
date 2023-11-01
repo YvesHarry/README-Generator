d461415717bbb948fb39c80b86e2b0c5807de46c
YvesHarry <yhdeslouches@hotmail.com>
2023-11-01 12:29:13 -0400

    Updated GenerateMardown

--- utils/GenerateMarkdown.js
+++ utils/GenerateMarkdown.js
@@ -1,5 +1,7 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+// example url https://shields.io/badge/license-MIT-green
+
 function renderLicenseBadge(license) {
     let badge = "";
   
@@ -10,7 +12,7 @@ function renderLicenseBadge(license) {
     return badge;
   }
   
-  // TODO: Create a function that returns the license link
+  // Create a function that returns the license link
   // If there is no license, return an empty string
   function renderLicenseLink(license) {
     let licenseLink;
@@ -37,7 +39,7 @@ function renderLicenseBadge(license) {
     return licenseLink;
   }
   
-  // TODO: Create a function that returns the license section of README
+  // Create a function that returns the license section of README
   // If there is no license, return an empty string
   function renderLicenseSection(license) {
     let licenseSect = "";
@@ -52,9 +54,8 @@ function renderLicenseBadge(license) {
     return licenseSect;
   }
   
-  // TODO: Create a function to generate markdown for README
+  // Create a function to generate markdown for README
   function generateMarkdown(data) {
-    
     const sections = ["Description", "Installation", "Usage", "Contributing", "Tests", "License", "Questions"];
   
     // add title
@@ -101,10 +102,6 @@ function renderLicenseBadge(license) {
     markdown += "You can email me at " + data.email + " if you have any additional questions.\n"
   
     return markdown;
-   // return `# ${data.title}
-    
-  
-  //;
   }
   
   module.exports = generateMarkdown;
